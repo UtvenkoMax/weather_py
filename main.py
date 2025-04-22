6b2e50a2bb3ea5d9b72cf75bcd7fb7a7efb99c4b
UtvenkoMax <utvenkom2008@gmail.com>
2025-04-22 22:11:57 +0300

    requests

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import requests
 
-name = 1 
-
-if name > 0:
-    print('hello world')
+def get_weather(city,api_key):
+    url = f"https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": api_key,
+        "units": "metric",  
+        "lang": "ua"
+    }
+    response = requests.get(url,params=params)
